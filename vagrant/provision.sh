c535db0a34b45c05bf3a36660b41f9cb87cda1aa
yutokyokutyo <k.yuto.cre@gmail.com>
2014-12-03 17:35:58 +0900

    Gemfileの内容を変更する際に無駄が多くリスクの高い処理をしていたので、ヒアドキュメントを用いてシンプルな処理に改善した

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -67,16 +67,69 @@ CHK_DIR_sample_app=/home/vagrant/sample_app
 
 if [ ! -d ${CHK_DIR_sample_app} ] ; then
     git clone https://github.com/yutokyokutyo/sample_app.git
-    cd /home/vagrant/sample_app/ ; rm Gemfile Gemfile.lock ; bundle init
 fi
 
-# Gemfileにgemを追記してbundlerでインストールする
-CHK_FILE_gemfile=/home/vagrant/sampleapp_provisioning_gemfile/Gemfile
 
-if [ ! -f ${CHK_FILE_gemfile} ] ; then
-     git clone https://github.com/yutokyokutyo/sampleapp_provisioning_gemfile.git
-     cp -f /home/vagrant/sampleapp_provisioning_gemfile/Gemfile /home/vagrant/sample_app/Gemfile
-fi
+
+
+
+
+
+
+
+# Gemfileの内容を変更し、bundlerでインストールする
+cat <<'EOS' > /home/vagrant/sample_app/Gemfile
+source 'https://rubygems.org'
+ruby '2.1.5'
+#ruby-gemset=railstutorial_rails_4_0
+
+gem 'rails', '4.0.8'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
+gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
+gem 'libv8'
+gem 'execjs'
+gem 'therubyracer', :platforms => :ruby
+gem 'nokogiri', '1.6.4.1'
+gem 'rbenv-rehash'
+
+group :development, :test do
+  gem 'sqlite3', '1.3.8'
+  gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.6'
+  gem 'serverspec'
+end
+
+group :test do
+  gem 'selenium-webdriver', '2.35.1'
+  gem 'capybara', '2.1.0'
+  gem 'factory_girl_rails', '4.2.0'
+  gem 'cucumber-rails', '1.4.0', :require => false
+  gem 'database_cleaner', github: 'bmabey/database_cleaner'
+end
+
+gem 'sass-rails', '4.0.1'
+gem 'uglifier', '2.1.1'
+gem 'coffee-rails', '4.0.1'
+gem 'jquery-rails', '3.0.4'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '1.0.2'
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
+
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor', '0.0.2'
+end
+EOS
 cd /home/vagrant/sample_app/ ; bundle install --without production --path /home/vagrant/sample_app/vendor/bundle
 
 # iptablesの設定
