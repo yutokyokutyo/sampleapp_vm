95e0c490c68ec02b0d01025d77d716b31a6ead90
yutokyokutyo <k.yuto.cre@gmail.com>
2014-12-03 13:11:56 +0900

    CHK_DIR という変数に複数の違う値を挿入していたため、値ごとに変数名を変更する仕様にしました

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -21,9 +21,9 @@ packages=(
 sudo yum -y install "${packages[@]}"
 
 # rbenvをcloneする
-CHK_DIR=/home/vagrant/.rbenv
+CHK_DIR_rbenv=/home/vagrant/.rbenv
 
-if [ ! -d ${CHK_DIR} ] ; then
+if [ ! -d ${CHK_DIR_rbenv} ] ; then
     git clone https://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
     # rbenvの環境設定
     echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bash_profile
@@ -32,48 +32,48 @@ if [ ! -d ${CHK_DIR} ] ; then
 fi
 
 # ruby-buildをcloneする
-CHK_DIR=/home/vagrant/.rbenv/plugins/ruby-build
+CHK_DIR_ruby_build=/home/vagrant/.rbenv/plugins/ruby-build
 
-if [ ! -d ${CHK_DIR} ] ; then
+if [ ! -d ${CHK_DIR_ruby_build} ] ; then
     git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
 fi
 
 # Rubyをインストールする
-CHK_DIR=/home/vagrant/.rbenv/versions/2.1.5
+CHK_DIR_ruby=/home/vagrant/.rbenv/versions/2.1.5
 
-if [ ! -d ${CHK_DIR} ] ; then
+if [ ! -d ${CHK_DIR_ruby} ] ; then
      CONFIGURE_OPTS="--disable-install-doc" rbenv install 2.1.5
      rbenv global 2.1.5
 fi
 
 # .gemrcの作成とrdoc,riをいれない設定
-CHK_FILE=/home/vagrant/.gemrc
+CHK_FILE_gemrc=/home/vagrant/.gemrc
 
-if [ ! -f ${CHK_FILE} ] ; then
+if [ ! -f ${CHK_FILE_gemrc} ] ; then
     touch /home/vagrant/.gemrc
     echo 'install: --no-rdoc --no-ri' >> /home/vagrant/.gemrc
     echo 'update: --no-rdoc --no-ri' >> /home/vagrant/.gemrc
 fi
 
 # bundlerをインストールする
-CHK_FILE=/home/vagrant/.rbenv/shims/bundle
+CHK_FILE_bundle=/home/vagrant/.rbenv/shims/bundle
 
-if [ ! -f ${CHK_FILE} ] ; then
+if [ ! -f ${CHK_FILE_bundle} ] ; then
     gem install bundler
 fi
 
 # Sample_appを git clone する
-CHK_DIR=/home/vagrant/sample_app
+CHK_DIR_sample_app=/home/vagrant/sample_app
 
-if [ ! -d ${CHK_DIR} ] ; then
+if [ ! -d ${CHK_DIR_sample_app} ] ; then
     git clone https://github.com/yutokyokutyo/sample_app.git
     cd /home/vagrant/sample_app/ ; rm Gemfile Gemfile.lock ; bundle init
 fi
 
 # Gemfileにgemを追記してbundlerでインストールする
-CHK_FILE=/home/vagrant/sampleapp_provisioning_gemfile/Gemfile
+CHK_FILE_gemfile=/home/vagrant/sampleapp_provisioning_gemfile/Gemfile
 
-if [ ! -f ${CHK_FILE} ] ; then
+if [ ! -f ${CHK_FILE_gemfile} ] ; then
      git clone https://github.com/yutokyokutyo/sampleapp_provisioning_gemfile.git
      cp -f /home/vagrant/sampleapp_provisioning_gemfile/Gemfile /home/vagrant/sample_app/Gemfile
 fi
