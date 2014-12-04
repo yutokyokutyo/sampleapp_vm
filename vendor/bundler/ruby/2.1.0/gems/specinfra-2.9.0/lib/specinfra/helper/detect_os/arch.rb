e3bfea7d38e177a18cccdd9433d9199a7bc0b3a1
yutokyokutyo <k.yuto.cre@gmail.com>
2014-12-04 19:23:41 +0900

    不要なファイルを commit してしまったため、これらを削除しました

--- vendor/bundler/ruby/2.1.0/gems/specinfra-2.9.0/lib/specinfra/helper/detect_os/arch.rb
+++
@@ -1,7 +0,0 @@
-class Specinfra::Helper::DetectOs::Arch < Specinfra::Helper::DetectOs
-  def self.detect
-    if run_command('ls /etc/arch-release').success?
-      { :family => 'arch', :release => nil }
-    end
-  end
-end
