059983bfdd6bc33e90aa75fa71d0b0d6ff22abca
yutokyokutyo <k.yuto.cre@gmail.com>
2014-12-11 15:20:04 +0900

    package のインストールについて manifest を追加しました

---
+++ vagrant/init.pp
@@ -0,0 +1,19 @@
+package {
+[
+'patch',
+'git',
+'openssl-devel',
+'readline-devel',
+'libxml2-devel',
+'libxslt-devel',
+'sqlite-devel',
+'gcc',
+'gcc-c++',
+'openssl',
+'readline',
+'libxml2',
+'libxslt',
+'sqlite',
+]:
+ensure => installed,
+}
